a724ef39ff22397781932142b4b13db7d5a84d2d
ELDER LUCAS <elderlucas@gmail.com>
2016-09-20 19:18:47 +0000

    Incluindo as alteracoes feitas no arquivo principal

--- project.py
+++ project.py
@@ -1,13 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-print "im ok"
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+import os
+from flask import make_response
+import requests
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 app = Flask(__name__)
+
+app_path = os.getcwd()
+os.chdir(app_path)
+
+
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World!'
+
+	#return "Hello World"
+	return render_template('main.html')
+
+@app.route('/artesanatos')
+def artesanatos():
+	return "ok show Artesanatos"
+
+
+@app.route('/kits')
+def kits():
+	return "ok show kits"
+
+@app.route('/contatos')
+def contatos():
+	return "ok show Contatos"
+
+@app.route('/about')
+def about():
+	return "ok show about Me"
+
 
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
