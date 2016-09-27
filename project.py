3c2771b7fe30dffb2b61eeb8e68341d79ff6f77f
Elder Lucas <elderlucas@gmail.com>
2016-09-27 20:23:12 -0300

    Alteracoes de layout e tbm relacionadas ao database

--- project.py
+++ project.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#Flask e SQLAlchemy
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from flask import session as login_session
+
+#Importando as classes criadas no setup do banco de dados.
+from setup_database import Base, User, Video_youtube, Video_metadata, Photo
+
+#Demais imports
 import random
 import string
 from oauth2client.client import flow_from_clientsecrets
@@ -24,30 +30,52 @@ app = Flask(__name__)
 app_path = os.getcwd()
 os.chdir(app_path)
 
+# Variavel "GLOBAL" para nos auxiliar com o PRINT DEINFORMAÇÕES RELEVANTES
+DEBUG_ALL = True		#Exibe todas as mensagens Globais caso esteja habilitado
+DEBUG_ADD = True		#Exibe todas as mensagens das partes de cadastro de novos Videos
+DEBUB_USER = True		#Exibe todas as mensagens do usuários logado
+
+############### LOGIN ################
+
+#Path do Database: user:password@localhost/dabase_name
+DBpath = "mysql://mariainesartes:artesanato03031963@localhost/mariainesartes_utf8" #TODO: AUTOMATIZAR ESSA LINHA
+
+engine = create_engine(DBpath) # an Engine, which the Session will use for connection resources
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine) # create a configured "Session" class
+session = DBSession() # create a Session
 
 
+# URL base. O principal conteúdo do site são os vídeos do Maria Ines Artes.
 @app.route('/')
-def hello_world():
-
-	#return "Hello World"
+@app.route('/videos/')
+def showVideos():
+	print "### SHOW ALL VIDEOS"
 	return render_template('main.html')
 
-@app.route('/artesanatos')
-def artesanatos():
-	return "ok show Artesanatos"
-
-
-@app.route('/kits')
-def kits():
-	return "ok show kits"
-
-@app.route('/contatos')
-def contatos():
-	return "ok show Contatos"
+# URL TODO. Aqui será implementada a parte de Compras do Site.
+@app.route('/shopping')
+def shopping():
+	return "EM BREVE UMA LOJA VIRTUAL COM PRODUTOS E ARTESANATOS"
 
 @app.route('/about')
 def about():
-	return "ok show about Me"
+	return "EM BREVE MAIS INFORMAÇÕES SOBRE O CANAL"
+
+# SISTEMA DE LOGIN
+@app.route('/login')
+def login():
+	return "EM BREVE VOCE PODERA SE LOGAR PARA ACESSAR MAIS CONTEUDOS"
+
+# 0001 Cadastro de novos Videos
+@app.route('/video/new/', methods=['GET', 'POST'])
+def newVideos():
+	return "EM BREVE VOCE PODERA SE LOGAR PARA ACESSAR MAIS CONTEUDOS"
+
+
+
+
 
 
 if __name__ == '__main__':
